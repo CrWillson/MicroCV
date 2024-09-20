997a31afc442b0dcaa28261e680563c1fe850df9
CrWillson <calebwillson1@gmail.com>
2024-09-20 19:21:11 -0400

    Added SSD1306 LCD control

---
+++ include/lcd.hpp
@@ -0,0 +1,68 @@
+#pragma once
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/// Helper functions for communicating with the LCD screen.
+///
+/// Author: Andrew Huffman
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Stdlib imports
+#include <vector>
+#include <string>
+
+// LCD imports
+#include "ssd1306.h"
+
+// Opencv Imports
+#undef EPS
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#define EPS 192
+
+namespace LCD {
+    /// @brief The native width of the screen.
+    constexpr uint8_t SCREEN_WIDTH = 128;
+
+
+    /// @brief The native heigth of the screen.
+    constexpr uint8_t SCREEN_HEIGHT = 64;
+
+    /// @brief If passed into one of the print functions as a row, prints oen after the previous.
+    constexpr int APPEND_ROW = -1;
+
+    /// @brief Draws a string to the LCD screen.
+    /// @param screen The screen to draw to.
+    /// @param string The string.
+    /// @param row The row of the screen to draw at.
+    void lcd_draw_string(SSD1306_t& screen, std::string& string, int row = APPEND_ROW);
+
+
+    /// @brief Writes a number of lines to the LCD screen, from top to bottom.
+    /// @param screen The screen to write to.
+    /// @param lines The lines to write to the screen.
+    /// @param start_row The row to start printing on.
+    void lcd_draw_string(SSD1306_t& screen, std::vector<std::string>& lines, int start_row = APPEND_ROW);
+
+
+    /// @brief Draws a string of this format in a row: "{preamble} {data}"
+    /// @param screen The scren to print to.
+    /// @param preamble The preamble.
+    /// @param data The data.
+    /// @param row The row on which to print.
+    void lcd_draw_data(SSD1306_t& screen, std::string preamble, int data, int row = APPEND_ROW);
+
+
+    /// @brief Draws a string of this format in a row: "{preamble} {data}"
+    /// @param screen The scren to print to.
+    /// @param preamble The preamble.
+    /// @param data The data.
+    /// @param row The row on which to print.
+    void lcd_draw_data(SSD1306_t& screen, std::string preamble, bool data, int row = APPEND_ROW);
+
+
+    /// @brief Writes an OpenCV matrix to the screen. This also clears the screen.
+    /// @param screen The screen to write to.
+    /// @param bin_mat The matrix to write. Should be a binary mask.
+    void lcd_draw_matrix(SSD1306_t& screen, const cv::Mat& bin_mat);
+}
---
+++ main/lcd.cpp
@@ -0,0 +1,65 @@
+#include "lcd.hpp"
+
+int curr_row = 0;
+
+void LCD::lcd_draw_string(SSD1306_t& screen, std::string& string, int row)
+{
+    if (-1 == row)
+    {
+        row = curr_row;
+    }
+
+    ssd1306_display_text(&screen, row, string.data(), string.size(), false);
+    curr_row = row + 1;
+}
+
+
+/// @brief Writes a parameter to the LCD screen.
+/// @param screen The screen to write to.
+/// @param lines The lines to write to the screen.
+void LCD::lcd_draw_string(SSD1306_t& screen, std::vector<std::string>& lines, int start_row)
+{
+    if (-1 == start_row)
+    {
+        start_row = curr_row;
+    }
+
+    for (size_t i = 0; i < lines.size(); i++)
+    {
+        std::string& str = lines[i];
+        lcd_draw_string(screen, str, start_row + i);
+    }
+    curr_row = lines.size() + start_row;
+}
+
+
+void LCD::lcd_draw_data(SSD1306_t& screen, std::string preamble, int data, int row)
+{
+    std::string arg = preamble + " " + std::to_string(data);
+    lcd_draw_string(screen, arg, row);
+}
+
+
+void LCD::lcd_draw_data(SSD1306_t& screen, std::string preamble, bool data, int row)
+{
+    std::string arg = preamble + " " + (data ? "true" : "false");
+    lcd_draw_string(screen, arg, row);
+}
+
+
+/// @brief Writes a binary matrix to a provided LCD screen.
+/// @param screen The LCD screen to write to.
+/// @param bin_mat The binary matrix to write.
+void LCD::lcd_draw_matrix(SSD1306_t& screen, const cv::Mat& bin_mat)
+{
+    curr_row = 0;
+    for (uint8_t row = 0; row < bin_mat.rows; row++)
+    {
+        for (uint8_t col = 0; col < bin_mat.cols; col++)
+        {
+            const bool invert = (0 == bin_mat.at<uint8_t>(row, col));
+            _ssd1306_pixel(&screen, col, row, invert);
+        }
+    }
+    ssd1306_show_buffer(&screen);
+}
