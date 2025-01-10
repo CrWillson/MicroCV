dd2e1000d6351fbbea8ec40801d1291fbc070392
CrWillson <calebwillson1@gmail.com>
2025-01-10 13:12:27 -0500

    Updated formatting

--- include/lcd.hpp
+++ include/lcd.hpp
@@ -24,68 +24,68 @@
 
 namespace LCD {
 
-/// @brief Parameters which are available to the LCD screen printing.
-class PrintParams {
-public:
-    PrintParams(): frame(cv::Mat1b()), start_tick(0), dist(0), stop_detected(false), car_detected(false) {}
+    /// @brief Parameters which are available to the LCD screen printing.
+    class PrintParams {
+    public:
+        PrintParams(): frame(cv::Mat1b()), start_tick(0), dist(0), stop_detected(false), car_detected(false) {}
 
-    cv::Mat1b frame;                ///< The image to print to the screen.
-    TickType_t start_tick;          ///< The number of ticks which have thus far passed.
-    int8_t dist;    ///< The number of pixels the line on the screen is from its ideal, calibrated position.
-    int8_t height;
-    bool stop_detected;             ///< Whether the stop line has been detected.
-    bool car_detected;              ///< Whether a car has been detected
-};
-
-
-/// @brief The native width of the screen.
-constexpr uint8_t SCREEN_WIDTH = 128;
+        cv::Mat1b frame;                ///< The image to print to the screen.
+        TickType_t start_tick;          ///< The number of ticks which have thus far passed.
+        int8_t dist;    ///< The number of pixels the line on the screen is from its ideal, calibrated position.
+        int8_t height;
+        bool stop_detected;             ///< Whether the stop line has been detected.
+        bool car_detected;              ///< Whether a car has been detected
+    };
 
 
-/// @brief The native heigth of the screen.
-constexpr uint8_t SCREEN_HEIGHT = 64;
+    /// @brief The native width of the screen.
+    constexpr uint8_t SCREEN_WIDTH = 128;
 
 
-/// @brief If passed into one of the print functions as a row, prints oen after the previous.
-constexpr int APPEND_ROW = -1;
+    /// @brief The native heigth of the screen.
+    constexpr uint8_t SCREEN_HEIGHT = 64;
 
 
-/// @brief Draws a string to the LCD screen.
-/// @param screen The screen to draw to.
-/// @param string The string.
-/// @param row The row of the screen to draw at.
-void lcd_draw_string(SSD1306_t& screen, std::string& string, int row = APPEND_ROW);
+    /// @brief If passed into one of the print functions as a row, prints oen after the previous.
+    constexpr int APPEND_ROW = -1;
 
 
-/// @brief Writes a number of lines to the LCD screen, from top to bottom.
-/// @param screen The screen to write to.
-/// @param lines The lines to write to the screen.
-/// @param start_row The row to start printing on.
-void lcd_draw_string(SSD1306_t& screen, std::vector<std::string>& lines, int start_row = APPEND_ROW);
+    /// @brief Draws a string to the LCD screen.
+    /// @param screen The screen to draw to.
+    /// @param string The string.
+    /// @param row The row of the screen to draw at.
+    void lcd_draw_string(SSD1306_t& screen, std::string& string, int row = APPEND_ROW);
 
 
-/// @brief Draws a string of this format in a row: "{preamble} {data}"
-/// @param screen The scren to print to.
-/// @param preamble The preamble.
-/// @param data The data.
-/// @param row The row on which to print.
-void lcd_draw_data(SSD1306_t& screen, std::string preamble, int data, int row = APPEND_ROW);
-void lcd_draw_data(SSD1306_t& screen, std::string preamble, double data, int row = APPEND_ROW);
+    /// @brief Writes a number of lines to the LCD screen, from top to bottom.
+    /// @param screen The screen to write to.
+    /// @param lines The lines to write to the screen.
+    /// @param start_row The row to start printing on.
+    void lcd_draw_string(SSD1306_t& screen, std::vector<std::string>& lines, int start_row = APPEND_ROW);
 
 
-/// @brief Draws a string of this format in a row: "{preamble} {data}"
-/// @param screen The scren to print to.
-/// @param preamble The preamble.
-/// @param data The data.
-/// @param row The row on which to print.
-void lcd_draw_data(SSD1306_t& screen, std::string preamble, bool data, int row = APPEND_ROW);
+    /// @brief Draws a string of this format in a row: "{preamble} {data}"
+    /// @param screen The scren to print to.
+    /// @param preamble The preamble.
+    /// @param data The data.
+    /// @param row The row on which to print.
+    void lcd_draw_data(SSD1306_t& screen, std::string preamble, int data, int row = APPEND_ROW);
+    void lcd_draw_data(SSD1306_t& screen, std::string preamble, double data, int row = APPEND_ROW);
 
 
-/// @brief Writes an OpenCV matrix to the screen. This also clears the screen.
-/// @param screen The screen to write to.
-/// @param bin_mat The matrix to write. Should be a binary mask.
-void lcd_draw_matrix(SSD1306_t& screen, const cv::Mat& bin_mat);
+    /// @brief Draws a string of this format in a row: "{preamble} {data}"
+    /// @param screen The scren to print to.
+    /// @param preamble The preamble.
+    /// @param data The data.
+    /// @param row The row on which to print.
+    void lcd_draw_data(SSD1306_t& screen, std::string preamble, bool data, int row = APPEND_ROW);
 
 
-void output_to_screen(SSD1306_t& screen, PrintParams& params);
+    /// @brief Writes an OpenCV matrix to the screen. This also clears the screen.
+    /// @param screen The screen to write to.
+    /// @param bin_mat The matrix to write. Should be a binary mask.
+    void lcd_draw_matrix(SSD1306_t& screen, const cv::Mat& bin_mat);
+
+
+    void output_to_screen(SSD1306_t& screen, PrintParams& params);
 }
