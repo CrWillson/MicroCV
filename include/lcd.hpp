6b4487c40bf4a2526d3227bb7c5516e216b40b63
CrWillson <calebwillson1@gmail.com>
2025-01-18 20:41:51 -0500

    Added basic thread safety

--- include/lcd.hpp
+++ include/lcd.hpp
@@ -11,57 +11,63 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <mutex>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
 
 // LCD imports
 #include "ssd1306.h"
 
 // Opencv Imports
-#undef EPS
-#include "opencv2/core.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#define EPS 192
+#include "opencv2.hpp"
+
 
 namespace LCD {
+
+    constexpr char *TAG = "SCREEN";
 
     /// @brief Parameters which are available to the LCD screen printing.
     class PrintParams {
     public:
-        PrintParams(): frame(cv::Mat1b()), start_tick(0), dist(0), stop_detected(false), car_detected(false) {}
+        PrintParams(): frame(cv::Mat1b()) {}
 
         cv::Mat1b frame;                ///< The image to print to the screen.
-        TickType_t start_tick;          ///< The number of ticks which have thus far passed.
-        int8_t dist;    ///< The number of pixels the line on the screen is from its ideal, calibrated position.
+        int loop_ticks;          ///< The number of ticks which have thus far passed.
+        int8_t dist;            ///< The number of pixels the line on the screen is from its ideal, calibrated position.
         int8_t height;
         bool stop_detected;             ///< Whether the stop line has been detected.
         bool car_detected;              ///< Whether a car has been detected
+
+        SSD1306_t* screen;
     };
 
 
     /// @brief The native width of the screen.
     constexpr uint8_t SCREEN_WIDTH = 128;
 
-
     /// @brief The native heigth of the screen.
     constexpr uint8_t SCREEN_HEIGHT = 64;
 
-
     /// @brief If passed into one of the print functions as a row, prints oen after the previous.
     constexpr int APPEND_ROW = -1;
+
+    /// @brief Lock used to prevent the main thread from drawing to the screen while it is busy
+    static std::mutex screenMutex;
 
 
     /// @brief Draws a string to the LCD screen.
     /// @param screen The screen to draw to.
     /// @param string The string.
     /// @param row The row of the screen to draw at.
-    void lcd_draw_string(SSD1306_t& screen, std::string& string, int row = APPEND_ROW);
+    void lcd_draw_string(SSD1306_t* screen, std::string& string, int row = APPEND_ROW);
 
 
     /// @brief Writes a number of lines to the LCD screen, from top to bottom.
     /// @param screen The screen to write to.
     /// @param lines The lines to write to the screen.
     /// @param start_row The row to start printing on.
-    void lcd_draw_string(SSD1306_t& screen, std::vector<std::string>& lines, int start_row = APPEND_ROW);
+    void lcd_draw_string(SSD1306_t* screen, std::vector<std::string>& lines, int start_row = APPEND_ROW);
 
 
     /// @brief Draws a string of this format in a row: "{preamble} {data}"
@@ -69,8 +75,8 @@ namespace LCD {
     /// @param preamble The preamble.
     /// @param data The data.
     /// @param row The row on which to print.
-    void lcd_draw_data(SSD1306_t& screen, std::string preamble, int data, int row = APPEND_ROW);
-    void lcd_draw_data(SSD1306_t& screen, std::string preamble, double data, int row = APPEND_ROW);
+    void lcd_draw_data(SSD1306_t* screen, std::string preamble, int data, int row = APPEND_ROW);
+    void lcd_draw_data(SSD1306_t* screen, std::string preamble, double data, int row = APPEND_ROW);
 
 
     /// @brief Draws a string of this format in a row: "{preamble} {data}"
@@ -78,14 +84,15 @@ namespace LCD {
     /// @param preamble The preamble.
     /// @param data The data.
     /// @param row The row on which to print.
-    void lcd_draw_data(SSD1306_t& screen, std::string preamble, bool data, int row = APPEND_ROW);
+    void lcd_draw_data(SSD1306_t* screen, std::string preamble, bool data, int row = APPEND_ROW);
 
 
     /// @brief Writes an OpenCV matrix to the screen. This also clears the screen.
     /// @param screen The screen to write to.
     /// @param bin_mat The matrix to write. Should be a binary mask.
-    void lcd_draw_matrix(SSD1306_t& screen, const cv::Mat& bin_mat);
+    void lcd_draw_matrix(SSD1306_t* screen, const cv::Mat& bin_mat);
 
 
-    void output_to_screen(SSD1306_t& screen, PrintParams& params);
+    void output_to_screen(PrintParams* params);
+
 }
